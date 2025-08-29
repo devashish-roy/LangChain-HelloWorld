031ace9de78e4a3240c2a2afb2c7573f34ee600b
Devashish Singha Roy <127662380+devashish-roy@users.noreply.github.com>
2025-08-29 19:41:49 +0200

    Commit environment setup

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def main():
+    print("Hello world")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+
+if __name__ ==  "__main__":
+
+    main()
+    
